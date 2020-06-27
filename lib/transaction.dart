d67ff323162cb1df7ec75cf2aa36c0002d4c58f4
Ilya Kozlov <ilya@kozlov.dev>
2020-06-27 14:19:10 +0600

    feat: transaction class is defined

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,1 +1,8 @@
-class Transaction {}
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({this.id, this.title, this.amount, this.date});
+}
